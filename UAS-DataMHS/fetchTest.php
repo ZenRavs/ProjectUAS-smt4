bdbbd76da524452cdcfdf03c758f03d20c303dc1
ZenRavs <zenravens3990x@gmail.com>
2024-07-08 23:37:40 +0700

    modified:   UAS-DataMHS/fetchTest.php 	modified:   UAS-DataMHS/supabaseConnect.php

--- UAS-DataMHS/fetchTest.php
+++ UAS-DataMHS/fetchTest.php
@@ -1,1 +1,18 @@
 <?php
+include 'supabaseConnect.php';
+try {
+    $result = pg_query($dbconn, "SELECT * FROM mahasiswa");
+
+    if (!$result) {
+        echo "Query failed: " . pg_last_error();
+        exit;
+    }
+    print_r($result);
+    while ($row = pg_fetch_assoc($result)) {
+        echo $row['nim'] . " - " . $row['nama'] . "<br />";
+    }
+    pg_free_result($result);
+    pg_close($dbconn);
+} catch (Exception $e) {
+    echo "Fetch Unsucesfull " . $e;
+}
--- UAS-DataMHS/supabaseConnect.php
+++ UAS-DataMHS/supabaseConnect.php
@@ -2,20 +2,21 @@
 //user=postgres.ppvsrugrszzrajvwwqzw password=[YOUR-PASSWORD] host=aws-0-ap-southeast-1.pooler.supabase.com port=6543 dbname=postgres
 $host = 'aws-0-ap-southeast-1.pooler.supabase.com';
 $port = '6543'; // Default PostgreSQL port
-$dbname = 'ProjectUAS';
+$dbname = 'postgres';
 $user = 'postgres.ppvsrugrszzrajvwwqzw';
 $password = 'ProjectUASsmt4';
+$sslmode = 'require';
 
 try {
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
-    $options = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-    ];
-    $pdo = new PDO($dsn, $user, $password, $options);
-    echo "Connected to Supabase database successfully!";
-} catch (PDOException $e) {
+    $connection_string = "host=$host port=$port dbname=$dbname user=$user password=$password sslmode=$sslmode";
+    $dbconn = pg_connect($connection_string);
+    if (!$dbconn) {
+        echo "Connection failed: " . pg_last_error();
+        exit;
+    } else {
+        echo "Connected to Supabase database successfully!<br />";
+    }
+} catch (Exception $e) {
     echo "Connection failed: " . $e->getMessage();
     phpinfo();
 }

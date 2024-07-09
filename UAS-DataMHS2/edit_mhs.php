2cecf4efafca694421e391a17a9a4eee6b738cd4
Userpc16 <khususpcsaja@gmail.com>
2024-07-09 22:48:10 +0700

    modified:   UAS-DataMHS2/edit_mhs.php

--- UAS-DataMHS2/edit_mhs.php
+++ UAS-DataMHS2/edit_mhs.php
@@ -1,38 +1,36 @@
 <?php
 include 'supabaseConnect.php';
 
+// Mengambil data dari form
 $id_mhs = $_POST['id_mhs'];
-$username_mhs = $_POST['username'];
-$password_mhs = $_POST['password'];
+$username_mhs = $_POST['username_mhs'];
+$password_mhs = $_POST['password_mhs'];
 $nim = $_POST['nim'];
-$nama_mhs = $_POST['nama'];
-$alamat_mhs = $_POST['alamat'];
-$telp_mhs = $_POST['telp'];
-$email_mhs = $_POST['email'];
+$nama_mhs = $_POST['nama_mhs'];
+$alamat_mhs = $_POST['alamat_mhs'];
+$telp_mhs = $_POST['telp_mhs'];
+$email_mhs = $_POST['email_mhs'];
 $tanggal_lahir = $_POST['tanggal_lahir'];
 
+// Menyiapkan pernyataan SQL untuk mengupdate data
 $sql = "UPDATE mahasiswa SET 
-    username='$username_mhs', 
-    password='$password_mhs', 
+    username_mhs='$username_mhs', 
+    password_mhs='$password_mhs', 
     nim='$nim', 
-    nama='$nama_mhs', 
-    alamat='$alamat_mhs', 
-    telp='$telp_mhs', 
-    email='$email_mhs', 
+    nama_mhs='$nama_mhs', 
+    alamat_mhs='$alamat_mhs', 
+    telp_mhs='$telp_mhs', 
+    email_mhs='$email_mhs', 
     tanggal_lahir='$tanggal_lahir' 
     WHERE id_mhs=$id_mhs";
 
+// Mengeksekusi pernyataan SQL
 if ($conn->query($sql) === TRUE) {
     echo "Record updated successfully";
 } else {
     echo "Error updating record: " . $conn->error;
 }
 
+// Menutup koneksi
 $conn->close();
 ?>
-
-<pre>
-<?php
-print_r($_POST);
-?>
-</pre>

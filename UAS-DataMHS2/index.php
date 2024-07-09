84af0c907609da996ec4fc2476bc42d6a49d846f
ZenRavs <zenravens3990x@gmail.com>
2024-07-09 21:47:10 +0700

    modified:   UAS-DataMHS2/index.php

--- UAS-DataMHS2/index.php
+++ UAS-DataMHS2/index.php
@@ -44,26 +44,14 @@ session_start();
                         </div>
                     </div>
                 </div>
-                <button type="button" class="btn btn-primary" id="liveToastBtn">Show live toast</button>
                 <?php
                 if (isset($_SESSION['login_error'])) {
                     $error = $_SESSION['login_error'];
                 ?>
-                    <div class="toast-container position-fixed bottom-0 end-0 p-3">
-                        <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-                            <div class="toast-header">
-                                <img src="..." class="rounded me-2" alt="...">
-                                <strong class="me-auto">Bootstrap</strong>
-                                <small>11 mins ago</small>
-                                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                            </div>
-                            <div class="toast-body">
-                                <?php
-                                echo 'Error: ' . $error;
-                                ?>
-
-                            </div>
-                        </div>
+                    <div class="alert alert-danger mt-3" role="alert">
+                        <?php
+                        echo 'Error: ' . $error;
+                        ?>
                     </div>
                 <?php
                 }

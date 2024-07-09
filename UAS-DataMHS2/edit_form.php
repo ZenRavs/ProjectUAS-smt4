021f83d0db001fa56e29281312c2b568e3696d9f
Userpc16 <khususpcsaja@gmail.com>
2024-07-10 00:43:28 +0700

    modified:   UAS-DataMHS2/edit_form.php

--- UAS-DataMHS2/edit_form.php
+++ UAS-DataMHS2/edit_form.php
@@ -1,7 +1,15 @@
 <?php
 include 'supabaseConnect.php';
-?>
+session_start();
 
+if (isset($_SESSION['user']) && isset($_GET['id_mhs'])) {
+    // Mengambil data mahasiswa berdasarkan ID
+    $id_mhs = $_GET['id_mhs'];
+    $query = "SELECT * FROM mahasiswa WHERE id_mhs = $id_mhs";
+    $result = pg_query($dbconn, $query);
+    $mhs = pg_fetch_assoc($result);
+    if ($mhs) {
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,43 +19,43 @@ include 'supabaseConnect.php';
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
+    <div class="container mt-3">
         <h2>Edit Data Mahasiswa</h2>
-        <form action="dashboard.php" method="post">
-            <input type="hidden" name="id_mhs" value="<?php echo $row['id_mhs']; ?>">
+        <form action="update_mahasiswa.php" method="post">
+            <input type="hidden" name="id_mhs" value="<?php echo $mhs['id_mhs']; ?>">
             <div class="form-group">
-                <label for="username_mhs">Username :</label>
-                <input type="text" class="form-control" id="username_mhs" name="username_mhs" value="<?php echo $row['username_mhs']; ?>">
+                <label for="username_mhs">Username:</label>
+                <input type="text" class="form-control" id="username_mhs" name="username_mhs" value="<?php echo $mhs['username']; ?>" required>
             </div>
             <div class="form-group">
                 <label for="password_mhs">Password:</label>
-                <input type="password" class="form-control" id="password_mhs" name="password_mhs" value="<?php echo $row['password_mhs']; ?>">
+                <input type="password" class="form-control" id="password_mhs" name="password_mhs" value="<?php echo $mhs['password']; ?>" required>
             </div>
             <div class="form-group">
                 <label for="nim">NIM:</label>
-                <input type="text" class="form-control" id="nim" name="nim" value="<?php echo $row['nim']; ?>">
+                <input type="text" class="form-control" id="nim" name="nim" value="<?php echo $mhs['nim']; ?>" required>
             </div>
             <div class="form-group">
                 <label for="nama_mhs">Nama:</label>
-                <input type="text" class="form-control" id="nama_mhs" name="nama_mhs" value="<?php echo $row['nama_mhs']; ?>">
+                <input type="text" class="form-control" id="nama_mhs" name="nama_mhs" value="<?php echo $mhs['nama']; ?>" required>
             </div>
             <div class="form-group">
                 <label for="alamat_mhs">Alamat:</label>
-                <textarea class="form-control" id="alamat_mhs" name="alamat_mhs"><?php echo $row['alamat_mhs']; ?></textarea>
+                <textarea class="form-control" id="alamat_mhs" name="alamat_mhs" required><?php echo $mhs['alamat']; ?></textarea>
             </div>
             <div class="form-group">
                 <label for="telp_mhs">Telepon:</label>
-                <input type="text" class="form-control" id="telp_mhs" name="telp_mhs" value="<?php echo $row['telp_mhs']; ?>">
+                <input type="text" class="form-control" id="telp_mhs" name="telp_mhs" value="<?php echo $mhs['telp']; ?>" required>
             </div>
             <div class="form-group">
                 <label for="email_mhs">Email:</label>
-                <input type="email" class="form-control" id="email_mhs" name="email_mhs" value="<?php echo $row['email_mhs']; ?>">
+                <input type="email" class="form-control" id="email_mhs" name="email_mhs" value="<?php echo $mhs['email']; ?>" required>
             </div>
             <div class="form-group">
                 <label for="tanggal_lahir">Tanggal Lahir:</label>
-                <input type="date" class="form-control" id="tanggal_lahir" name="tanggal_lahir" value="<?php echo $row['tanggal_lahir']; ?>">
+                <input type="date" class="form-control" id="tanggal_lahir" name="tanggal_lahir" value="<?php echo $mhs['tanggal_lahir']; ?>" required>
             </div>
-            <button type="submit" class="btn btn-primary">Update</button>
+            <button type="submit" class="btn btn-primary mt-3">Update</button>
         </form>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
@@ -55,3 +63,11 @@ include 'supabaseConnect.php';
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
+<?php
+    } else {
+        echo "Data tidak ditemukan.";
+    }
+} else {
+    echo "Anda tidak memiliki akses.";
+}
+?>

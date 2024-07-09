a9aa51decf4bf1f8134f5afacb6e7bb0dddadd2c
Userpc16 <khususpcsaja@gmail.com>
2024-07-09 20:34:29 +0700

    modified:   UAS-DataMHS2/edit_mhs.php

--- UAS-DataMHS2/edit_mhs.php
+++ UAS-DataMHS2/edit_mhs.php
@@ -2,14 +2,14 @@
 include 'supabaseConnect.php';
 session_start();
 $id_mhs = $_POST['id_mhs'];
-$username_mhs = $_POST['id_mhs'];
-$password_mhs = $_POST['id_mhs'];
-$nim = $_POST['id_mhs'];
-$nama_mhs = $_POST['id_mhs'];
-$alamat_mhs = $_POST['id_mhs'];
-$telp_mhs = $_POST['id_mhs'];
-$email_mhs = $_POST['id_mhs'];
-$tanggal_lahir = $_POST['id_mhs'];
+$username_mhs = $_POST['username_mhs'];
+$password_mhs = $_POST['password_mhs'];
+$nim = $_POST['nim'];
+$nama_mhs = $_POST['nama_mhs'];
+$alamat_mhs = $_POST['alamat_mhs'];
+$telp_mhs = $_POST['telp_mhs'];
+$email_mhs = $_POST['email.mhs'];
+$tanggal_lahir = $_POST['tanggal_lahir'];
 ?>
 <pre>
 <?php

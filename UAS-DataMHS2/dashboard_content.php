5872c852fc6692f1fd980e0cbf824d1faa541275
ZenRavs <zenravens3990x@gmail.com>
2024-07-09 23:24:23 +0700

    modified:   UAS-DataMHS2/dashboard_content.php

--- UAS-DataMHS2/dashboard_content.php
+++ UAS-DataMHS2/dashboard_content.php
@@ -13,10 +13,10 @@ $data_mhs = pg_fetch_all($result);
       <tr>
         <th scope="col" class="text-center">No.</th>
         <th scope="col">ID</th>
-        <th scope="col">Username</th>
-        <th scope="col">Password</th>
         <th scope="col">NIM</th>
         <th scope="col">Nama</th>
+        <th scope="col">Faklutas</th>
+        <th scope="col">Prodi</th>
         <th scope="col">Alamat</th>
         <th scope="col">No. Telp</th>
         <th scope="col">Email</th>
@@ -28,18 +28,18 @@ $data_mhs = pg_fetch_all($result);
       $i = 1;
       foreach ($data_mhs as $mhs) {
       ?>
-      <tr>
-        <td class="text-center"><?php echo $i ?></td>
-        <td><?php echo $mhs['id_mhs'] ?></td>
-        <td><?php echo $mhs['username'] ?></td>
-        <td><?php echo $mhs['password'] ?></td>
-        <td><?php echo $mhs['nim'] ?></td>
-        <td><?php echo $mhs['nama'] ?></td>
-        <td><?php echo $mhs['alamat'] ?></td>
-        <td><?php echo $mhs['telp'] ?></td>
-        <td><?php echo $mhs['email'] ?></td>
-        <td><?php echo $mhs['tanggal_lahir'] ?></td>
-      </tr>
+        <tr>
+          <td class="text-center"><?php echo $i ?></td>
+          <td><?php echo $mhs['id_mhs'] ?></td>
+          <td><?php echo $mhs['nim'] ?></td>
+          <td><?php echo $mhs['nama'] ?></td>
+          <td><?php echo $mhs['kode_fakultas'] ?></td>
+          <td><?php echo $mhs['kode_jurusan'] ?></td>
+          <td><?php echo $mhs['alamat'] ?></td>
+          <td><?php echo $mhs['telp'] ?></td>
+          <td><?php echo $mhs['email'] ?></td>
+          <td><?php echo $mhs['tanggal_lahir'] ?></td>
+        </tr>
       <?php
         $i++;
       }

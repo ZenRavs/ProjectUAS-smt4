2268a4c19af5ca918de16072076ce5498fc6e590
Userpc16 <khususpcsaja@gmail.com>
2024-07-10 14:17:09 +0700

    modified:   UAS-DataMHS2/insert_mhs.php

--- UAS-DataMHS2/insert_mhs.php
+++ UAS-DataMHS2/insert_mhs.php
@@ -3,10 +3,10 @@ include 'supabaseConnect.php';
 session_start();
 
 if (isset($_SESSION['user']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Mengambil data dari form
-    $nim = $_POST['nim_mhs'];
+    // Mengambil data dari formS
+    $nim = $_POST['nim'];
     $nama_mhs = $_POST['nama_mhs'];
-    $fakultas_mhs = $_POST['kode_faku'];
+    $fakultas_mhs = $_POST['username'];
     $jurusan_mhs = $_POST['kode_jurusan'];
     $alamat_mhs = $_POST['alamat_mhs'];
     $telp_mhs = $_POST['telp_mhs'];
@@ -17,12 +17,13 @@ if (isset($_SESSION['user']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
     $query = "INSERT INTO mahasiswa (nim, nama,kode_fakultas, kode_jurusan, alamat, telp, email, tanggal_lahir) 
               VALUES ('$nim', '$nama_mhs', '$fakultas_mhs', '$jurusan_mhs', '$alamat_mhs', '$telp_mhs', '$email_mhs', '$tanggal_lahir')";
 
-    $result = pg_update($dbconn, $table, $data, $condition);
-    // Mengeksekusi pernyataan SQL
+    $result = pg_insert($dbconn);
+
+    // Mengecek hasil eksekusi
     if ($result) {
-        echo "Record updated successfully";
+        echo "Record added successfully";
     } else {
-        echo "Error updating record: " . pg_last_error($dbconn);
+        echo "Error adding record: " . pg_last_error($dbconn);
     }
     header('location: dashboard.php');
     // Menutup koneksi

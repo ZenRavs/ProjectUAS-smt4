6df8e2a4075355e6175342c75ba6b55df305ba6d
ZenRavs <zenravens3990x@gmail.com>
2024-07-10 15:41:45 +0700

    modified:   UAS-DataMHS2/delete.php

--- UAS-DataMHS2/delete.php
+++ UAS-DataMHS2/delete.php
@@ -2,33 +2,22 @@
 include 'supabaseConnect.php';
 include 'delete_fungsi.php';
 session_start();
-<<<<<<< HEAD
-
-if (isset($_SESSION['user']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Mengambil ID mahasiswa dari form
-    $id_mhs = $_POST['id_mhs'];
-
-    // Memanggil fungsi delete_mahasiswa
-    if (delete($dbconn, $id_mhs)) {
-        echo "Record deleted successfully";
-=======
-$id_mhs = $_POST['id_mhs'];
+$id_mhs = ['id_mhs' => $_POST['id_mhs']];
 print_r($_POST);
 if (isset($_SESSION['user'])) {
     if (isset($_POST['id_mhs'])) {
+
         // Memanggil Query
         $result = pg_delete($dbconn, 'mahasiswa', $id_mhs);
         if ($result) {
             // Kembali ke Halaman setelah hapus
-            //header("Location: dashboard.php");
+            header("Location: dashboard.php");
         } else {
             echo "Error: Could not delete record.";
         }
->>>>>>> c00d476ba304ab275e5ed347cbafbfbd639d9aeb
     } else {
         echo "Error deleting record: " . pg_last_error($dbconn);
     }
-
     // Menutup koneksi
     pg_close($dbconn);
 } else {

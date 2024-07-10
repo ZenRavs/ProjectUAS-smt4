46d71b11afd20a746c0bcef580a802ec9da6640d
Userpc16 <khususpcsaja@gmail.com>
2024-07-10 21:32:17 +0700

    modified:   UAS-DataMHS2/fungsi_fakultas.php

--- UAS-DataMHS2/fungsi_fakultas.php
+++ UAS-DataMHS2/fungsi_fakultas.php
@@ -2,20 +2,17 @@
 include 'supabaseConnect.php';
 session_start();
 
-if (isset($_POST['kode_faku'])) {
-    $kode_faku = $_POST['kode_faku'];
-    $kode_jurusan = $_POST['kode_jurusan'];
+if (isset($_SESSION['user'])) {
+    $fakultas_mhs = $_POST['kode_faku'];
+    $jurusan_mhs = $_POST['kode_jurusan'];
     print_r ($_POST);
 
-    $query = "UPDATE fakultas SET kode_jurusan = '$kode_jurusan' WHERE kode_faku = $kode_faku";
-    $result = pg_query($dbconn, $query);
-
-    if ($result) {
-        echo 'success';
-    } else {
-        echo 'error';
-    }
+    $table = 'fakultas';
+    $data = [
+        'kode_faku' => $fakultas_mhs,
+        'kode_jurusan' => $jurusan_mhs,
+    ];
+    $result = pg_insert($dbconn, $table, $data);
 } else {
-    echo 'Ada kesalahan.';
+    echo "Invalid request.";
 }
-?>

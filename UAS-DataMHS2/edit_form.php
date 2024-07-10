a0c96a2a7610f9cb8f5b7df3a1d2ae706b8c0112
atharyanm <r.atha.m025@gmail.com>
2024-07-10 16:13:14 +0700

    modified:   UAS-DataMHS2/edit_form.php 	modified:   UAS-DataMHS2/edit_mhs.php 	modified:   UAS-DataMHS2/table_mhs.php

--- UAS-DataMHS2/edit_form.php
+++ UAS-DataMHS2/edit_form.php
@@ -8,74 +8,69 @@ if (isset($_SESSION['user'])) {
     $mhs = pg_fetch_assoc($result);
 ?>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Edit Mahasiswa</title>
-        <link href="https://maxcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    </head>
-
-    <body>
-        <div class="container mt-3 mb-3">
-            <div class="row justify-content-center">
-                <div class="col-lg-10">
-                    <div class="card shadow">
-                        <div class="card-header">
-                            <h2 class="text-primary"> <?php echo $mhs['nama'] ?></h2>
+<div class="container mt-3 mb-3">
+    <div class="row justify-content-center">
+        <div class="col-lg-10">
+            <div class="card shadow">
+                <div class="card-header">
+                    <h2 class="text-primary"> <?php echo $mhs['nama'] ?></h2>
+                </div>
+                <div class="card-body">
+                    <form id="editForm" method="post">
+                        <input type="hidden" name="id_mhs" value="<?php echo $id_mhs ?>">
+                        <div class="form-group">
+                            <div class="row flex">
+                                <div class="col-md-2">
+                                    <label for="username">Fakultas</label>
+                                    <input type="text" class="form-control" id="kode_faku" name="kode_faku" value="<?php echo $mhs['kode_fakultas']; ?>" required>
+                                </div>
+                                <div class="col-md-2">
+                                    <label for="password_mhs">Jurusan</label>
+                                    <input type="text" class="form-control" id="kode_jurusan" name="kode_jurusan" value="<?php echo $mhs['kode_jurusan']; ?>" required>
+                                </div>
+                                <div class="col-md">
+                                    <label for="nim">NIM</label>
+                                    <input type="text" class="form-control" id="nim_mhs" name="nim_mhs" value="<?php echo $mhs['nim']; ?>" required>
+                                </div>
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <form id="editForm" method="post">
-                                <input type="hidden" name="id_mhs" value="<?php echo $id_mhs ?>">
-                                <div class="form-group">
-                                    <div class="row flex">
-                                        <div class="col-md-2">
-                                            <label for="username">Faklutas</label>
-                                            <input type="text" class="form-control" id="kode_faku" name="kode_faku" value="<?php echo $mhs['kode_fakultas']; ?>" required>
-                                        </div>
-                                        <div class="col-md-2">
-                                            <label for="password_mhs">Jurusan</label>
-                                            <input type="text" class="form-control" id="kode_jurusan" name="kode_jurusan" value="<?php echo $mhs['kode_jurusan']; ?>" required>
-                                        </div>
-                                        <div class="col-md">
-                                            <label for="nim">NIM</label>
-                                            <input type="text" class="form-control" id="nim_mhs" name="nim_mhs" value="<?php echo $mhs['nim']; ?>" required>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="nama_mhs">Nama</label>
-                                    <input type="text" class="form-control" id="nama_mhs" name="nama_mhs" value="<?php echo $mhs['nama']; ?>" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="alamat_mhs">Alamat</label>
-                                    <textarea class="form-control" id="alamat_mhs" name="alamat_mhs" required><?php echo $mhs['alamat']; ?></textarea>
-                                </div>
-                                <div class="form-group">
-                                    <label for="telp_mhs">Telepon</label>
-                                    <input type="text" class="form-control" id="telp_mhs" name="telp_mhs" value="<?php echo $mhs['telp']; ?>" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="email_mhs">Email</label>
-                                    <input type="email" class="form-control" id="email_mhs" name="email_mhs" value="<?php echo $mhs['email']; ?>" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="tanggal_lahir">Tanggal Lahir</label>
-                                    <input type="date" class="form-control" id="tanggal_lahir" name="tanggal_lahir" value="<?php echo $mhs['tanggal_lahir']; ?>" required>
-                                </div>
-                                <div class="col mt-4">
-                                    <button type="button" class="btn btn-danger mr-3">Cancel</button>
-                                    <button type="submit" class="btn btn-primary">Update</button>
-                                </div>
-                            </form>
+                        <div class="form-group">
+                            <label for="nama_mhs">Nama</label>
+                            <input type="text" class="form-control" id="nama_mhs" name="nama_mhs" value="<?php echo $mhs['nama']; ?>" required>
                         </div>
-                    </div>
+                        <div class="form-group">
+                            <label for="alamat_mhs">Alamat</label>
+                            <textarea class="form-control" id="alamat_mhs" name="alamat_mhs" required><?php echo $mhs['alamat']; ?></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="telp_mhs">Telepon</label>
+                            <input type="text" class="form-control" id="telp_mhs" name="telp_mhs" value="<?php echo $mhs['telp']; ?>" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="email_mhs">Email</label>
+                            <input type="email" class="form-control" id="email_mhs" name="email_mhs" value="<?php echo $mhs['email']; ?>" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="tanggal_lahir">Tanggal Lahir</label>
+                            <input type="date" class="form-control" id="tanggal_lahir" name="tanggal_lahir" value="<?php echo $mhs['tanggal_lahir']; ?>" required>
+                        </div>
+                        <div class="col mt-4">
+                            <button type="button" class="btn btn-danger mr-3 cancel-btn">Cancel</button>
+                            <button type="submit" class="btn btn-primary">Update</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-        <script src="https://maxcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    </body>
+    </div>
+</div>
+<script>
+    $(document).ready(function() {
+        $('.cancel-btn').on('click', function() {
+            $('.content-inner').load('table_mhs.php');
+        });
+    });
+</script>
 <?php
 } else {
     echo "Anda tidak memiliki akses.";
--- UAS-DataMHS2/table_mhs.php
+++ UAS-DataMHS2/table_mhs.php
@@ -50,8 +50,7 @@ if (isset($_SESSION['user'])) {
                                         <button class="btn btn-sm btn-primary edit-btn" data-id="<?php echo $mhs['id_mhs'] ?>">Edit</button>
                                     </div>
                                     <div class="col md-0">
-                                        <form action="delete.php" method="POST">
-                                            <input type="hidden" name="id_mhs" id="<?php echo $mhs['id_mhs'] ?>" value="<?php echo $mhs['id_mhs'] ?>">
+                                        <form action="delete.php" method="get">
                                             <input class="btn btn-sm btn-danger" type="submit" value="Delete">
                                         </form>
                                     </div>
@@ -78,3 +77,4 @@ if (isset($_SESSION['user'])) {
 } else {
     header("location: index.php");
 }
+?>
